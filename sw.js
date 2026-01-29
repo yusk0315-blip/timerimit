e1b90bca618573ab7f3e6aca43fc68ba81d6748a
yusk0315-blip <yu.sk0315@gmail.com>
2026-01-29 22:01:46 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "free-time-life-v1";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./sw.js","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("./")))
+  );
+});
